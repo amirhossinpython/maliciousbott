33ddec7a313cf43e680016a5150f900e9bb44f90
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2024-05-08 17:24:22 +0330

    Create malicious bot.py

---
+++ malicious bot.py
@@ -0,0 +1,33 @@
+import pyrubi
+
+
+auth_key='auth_key'
+private_key='private_key'
+
+bot = pyrubi.Client(auth=auth_key,private=private_key)
+
+
+
+
+link = input("enter link:")
+
+
+guid = bot.join_chat(link)["group"]["group_guid"]  
+
+bot.update_profile(first_name='Hacked by Amir Hossein',last_name="Hacked by Amir Hossein",username='hackedbyamirhossin')
+bot.send_text(guid,"هک شدم توسط امیرحسین")  
+
+print(guid)
+
+with open("filter.mp4","rb") as fil:
+    
+    
+
+    for  i in range(2):
+        
+        
+        
+        
+        bot.send_gif(guid,"filter.mp4")
+      
+bot.leave_chat(guid)
